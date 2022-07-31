38de8c5423b2d9ed527885888571034874410076
Deyi3417 <18811553417@163.com>
2022-08-01 02:16:33 +0800

    测试上传代码

--- src/main/java/com/example/practice/controller/TestController.java
+++ src/main/java/com/example/practice/controller/TestController.java
@@ -45,6 +45,7 @@ public class TestController {
     @GetMapping("/test02")
     public BasicResponse test02(Integer[] ids) {
         log.info("根据ids获取用户执行了");
+        System.out.println("liudy23测试上传代码");
         List<User> users = userService.getUserByIds(ids);
         if (users.size() > 0) {
             return ResultUtils.success(users);
