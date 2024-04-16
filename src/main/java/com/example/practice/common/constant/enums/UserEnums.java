2ffe86f45793e03f868a674c9c2720bff52af22c
Deyi3417 <18811553417@163.com>
2024-04-16 22:47:39 +0800

    优化导入

--- src/main/java/com/example/practice/common/constant/enums/UserEnums.java
+++ src/main/java/com/example/practice/common/constant/enums/UserEnums.java
@@ -1,16 +1,14 @@
 package com.example.practice.common.constant.enums;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
-
  * 用户枚举类
+ *
  * @author : liudy23
  * @data : 2023/5/23
  */
@@ -20,10 +18,11 @@ public class UserEnums {
     @AllArgsConstructor
     @NoArgsConstructor
     public enum UserGenderEnum {
-        MAN(0,"男"),
-        WOMAN(1,"女");
+        MAN(0, "男"),
+        WOMAN(1, "女");
         private Integer code;
         private String name;
+
         public static UserGenderEnum byCode(Integer code) {
             return Arrays.stream(values()).filter(e -> e.getCode().equals(code)).findFirst().orElse(MAN);
         }
--- src/main/java/com/example/practice/controller/DownloadController.java
+++ src/main/java/com/example/practice/controller/DownloadController.java
@@ -5,7 +5,10 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
@@ -56,6 +59,4 @@ public class DownloadController {
             log.error(e.getMessage());
         }
     }
-
-
 }
